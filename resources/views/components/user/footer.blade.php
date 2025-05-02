2fd870a7bf812a25f146a12c6a0f80f43aa56451
IndraRusyana <indrarusyana@gmail.com>
2025-05-02 07:10:26 +0700

    inheritance home

---
+++ resources/views/components/user/footer.blade.php
@@ -0,0 +1,103 @@
+<div class="site-footer">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-3 pe-4">
+                <!-- Content -->
+                <img class="mb-4 img-fluid w-50" src="assets/user/images/LOGO_SAUDINESIA.png" alt="" srcset="">
+                <p>
+                    Merupakan salah satu perusahaan yang bergerak di bidang usaha perdagangan tekstil, konveksi, supplier, perdagangan umum, dan konstruksi sipil.
+                </p>
+                <div class="widget">
+                    <h3>Connect</h3>
+                    <ul class="list-unstyled social">
+                        <li><a href="#"><span class="icon-instagram"></span></a></li>
+                        <li><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li><a href="#"><span class="icon-linkedin"></span></a></li>
+                        <li><a href="#"><span class="icon-pinterest"></span></a></li>
+                        <li><a href="#"><span class="icon-dribbble"></span></a></li>
+                    </ul>
+                </div> <!-- /.widget -->
+            </div>
+            {{-- <div class="col-lg-4">
+                <div class="widget">
+                    <a href="index.html" class="logo m-0 float-start"><img src="assets/user/images/LOGO_SAUDINESIA.png" alt="" srcset="" width="120"></a>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                </div> <!-- /.widget -->
+                <div class="widget">
+                    <h3>Connect</h3>
+                    <ul class="list-unstyled social">
+                        <li><a href="#"><span class="icon-instagram"></span></a></li>
+                        <li><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li><a href="#"><span class="icon-linkedin"></span></a></li>
+                        <li><a href="#"><span class="icon-pinterest"></span></a></li>
+                        <li><a href="#"><span class="icon-dribbble"></span></a></li>
+                    </ul>
+                </div> <!-- /.widget -->
+            </div> <!-- /.col-lg-3 --> --}}
+
+            <div class="col-lg-2 ml-auto">
+                <div class="widget">
+                    <h3>Paket</h3>
+                    <ul class="list-unstyled float-left links">
+                        <li><a href="#">Haji</a></li>
+                        <li><a href="#">Umroh</a></li>
+                    </ul>
+                </div> <!-- /.widget -->
+            </div> <!-- /.col-lg-3 -->
+
+            <div class="col-lg-2">
+                <div class="widget">
+                    <h3>Service</h3>
+                    <ul class="list-unstyled float-left links">
+                        <li><a href="#">Hotel</a></li>
+                        <li><a href="#">Transport</a></li>
+                        <li><a href="#">Muttowif</a></li>
+                        <li><a href="#">Visa</a></li>
+                    </ul>
+                </div> <!-- /.widget -->
+            </div> <!-- /.col-lg-3 -->
+
+            <div class="col-lg-2">
+                <div class="widget">
+                    <h3>Links</h3>
+                    <ul class="list-unstyled float-left links">
+                        <li><a href="#">Informasi</a></li>
+                        <li><a href="#">Tentang kami</a></li>
+                        <li><a href="#">Helpdesk</a></li>
+                    </ul>
+                </div> <!-- /.widget -->
+            </div> <!-- /.col-lg-3 -->
+
+            <div class="col-lg-3">
+                <div class="widget">
+                    <h3>Map</h3>
+                    <address>43 Raymouth Rd. Baltemoer, London 3910</address>
+                    <ul class="list-unstyled links mb-4">
+                        <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+                        <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+                        <li><a href="mailto:info@mydomain.com">info@mydomain.com</a></li>
+                    </ul>
+                </div> <!-- /.widget -->
+            </div> <!-- /.col-lg-3 -->
+
+        </div> <!-- /.row -->
+
+        <div class="row mt-5">
+            <div class="col-12 text-center">
+                <p class="mb-0">Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved.
+                </p>
+            </div>
+        </div> <!-- /.container -->
+    </div> <!-- /.site-footer -->
+
+    <!-- Preloader -->
+    <div id="overlayer"></div>
+    <div class="loader">
+        <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</div>
